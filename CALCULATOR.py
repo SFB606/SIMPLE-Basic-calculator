42bd59724ae54befb85e6674d0c598283bdd5253
FAJR KK <132988085+SFB606@users.noreply.github.com>
2023-09-05 11:49:52 -0700

    Add files via upload
    This is just simple and very basic form of Calculator

---
+++ CALCULATOR.py
@@ -0,0 +1,38 @@
+
+
+while True:
+        a=int(input("enter your choice \n1.add \n2.sub \n3.mult \n4.div"))
+        c=input("do you want to continue")
+        if c=='no':
+            break
+        else:
+            b1=int(input("enter your number"))
+            b2=int(input("enter your number"))
+            if a==1:
+                print(b1+b2)
+            elif a==2:
+                   print(b1-b2)
+            elif a==3:
+                print(b1*b2)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
